e6d7c210590cd89a98f47aa16753c5c109ea2ba1
EdotJ <eligijus.jocius@gmail.com>
2021-02-03 21:14:16 +0200

    Initial commit with init api project

---
+++ api/src/test/java/com/budgeteer/api/ApiTest.java
@@ -0,0 +1,22 @@
+package com.budgeteer.api;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import javax.inject.Inject;
+
+@MicronautTest
+class ApiTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+        Assertions.assertTrue(application.getApplicationContext().getEnvironment().getActiveNames().contains("test"));
+    }
+
+}
