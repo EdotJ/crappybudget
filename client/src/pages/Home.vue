33bdd4dc57d5d5bb7f369d1668bf45e9f8437500
EdotJ <eligijus.jocius+github@gmail.com>
2021-02-28 20:23:12 +0200

    Remove test components

--- client/src/pages/Home.vue
+++ client/src/pages/Home.vue
@@ -4,10 +4,6 @@
     <h2>Hello, World from Home!!</h2>
     <router-link to="/login" v-if="!isLoggedIn">Login</router-link>
     <router-link to="/register" v-if="!isLoggedIn">Register</router-link>
-    <router-link to="/accounts">Accounts</router-link>
-    <router-link to="/categories">Categories</router-link>
-    <router-link to="/goals">Goals</router-link>
-    <router-link to="/entries">Entries</router-link>
   </div>
 </template>
 
